13808241455bd8c59ba087dfa5787a95e43fdc61
Tad Johnston <tjohnston@rentpath.com>
2016-08-12 14:42:09 -0700

    Adding simpler demo

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root 'mapbox#index'
 
   get '/working', to: 'mapbox#working'
+  get '/simple', to: 'mapbox#simple'
 end
--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,12 @@ config.entry = {
 };
 
 config.module.loaders.push({
+            test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file" },
+            { test: /\.(woff|woff2)$/, loader:"url?prefix=font/&limit=5000" },
+            { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/octet-stream" },
+            { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=image/svg+xml" }
+
+,{
     test: /\.js$/,
     include: path.resolve(__dirname, 'node_modules/webworkify/index.js'),
     loader: 'worker'
